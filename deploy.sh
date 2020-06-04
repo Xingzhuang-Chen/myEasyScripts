0b89c97a230be77a8873fba96fff729cc971d778
Xingzhuang Chen <xzh.chen@hotmail.com>
2020-06-04 22:39:56 +0800

    添加deploy中添加condaenv部署，condaenv增加source调用检查

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,28 @@
 script_path=/usr/bin
 
 chmod +x switch-proxy.sh
-ln -s `pwd`/switch-proxy.sh $script_path/switch-proxy
+file_path=$script_path/switch-proxy
+if [ -f $file_path ]
+then
+    echo "$file_path exist. delete."
+    rm $file_path
+fi
+ln -s `pwd`/switch-proxy.sh $file_path
 
 chmod +x with-proxy.sh
-ln -s `pwd`/with-proxy.sh $script_path/with-proxy
+file_path=$script_path/with-proxy
+if [ -f $file_path ]
+then
+    echo "$file_path exist. delete."
+    rm $file_path
+fi
+ln -s `pwd`/with-proxy.sh $file_path
+
+chmod +x condaenv.sh
+file_path=$script_path/condaenv
+if [ -f $file_path ]
+then
+    echo "$file_path exist. delete."
+    rm $file_path
+fi
+ln -s `pwd`/condaenv.sh $file_path
